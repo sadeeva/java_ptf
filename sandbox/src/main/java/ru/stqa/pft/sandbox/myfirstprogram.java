657622430d8659301d80258114bebb99267b84de
sadeeva <k.sadeeva@gmail.com>
2018-03-07 16:01:42 -0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myfirstprogram.java
@@ -1,10 +1,34 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+
 public class Myfirstprogram {
 
     public static void main(String[] args) {
+        hello("World");
+        hello("User");
 
-        System.out.println("Hello, world!");
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l) );
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоyгольника со сторонами " + a + " и " + b + " = " + area(a,b) );
+
     }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+
+    }
+
+    public static double area ( double a, double b){
+        return a * b;
+    }
+
 
 }
